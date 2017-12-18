22719299b07efb4575d85815f54d7f61660ee2b3
Joscelyn James <josce.james7@hotmail.com>
2017-12-18 12:33:12 -0700

    extracted shutdown into pacakge shutdown

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"net/http"
-	"context"
 	"log"
 	"os"
 	"os/signal"
-	// "time"
 	h "github.com/joscelynjames/go-hashing/handlers"
-	// s "github.com/joscelynjames/go-hashing/shutdown"
+	s "github.com/joscelynjames/go-hashing/shutdown"
 )
 
 func main() {
@@ -18,9 +16,6 @@ func main() {
 	
 	mux := http.NewServeMux()
 	port := ":8081"
-
-	mux.HandleFunc("/", h.MakePosty)
-
 	srv := &http.Server{Addr: port, Handler: mux}
 
 	go func() {
@@ -30,15 +25,10 @@ func main() {
 			log.Printf("listen: %s\n", err)
 		}
 	}()
+	
+	mux.HandleFunc("/", h.MakePosty)
 
 	<-stopChan // wait for server to get kill signal (SIGINT)
-	log.Println("Shutting down server...")
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 	
-	srv.Shutdown(ctx)
-
-	log.Println("gracefully stopped stopped")
-
-	http.ListenAndServe(":8081", nil)
+	s.GracefullyShutdown(srv)
 }
---
+++ shutdown/shutdown.go
@@ -0,0 +1,17 @@
+package shutdown
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+func GracefullyShutdown(srv *http.Server) {
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	
+	srv.Shutdown(ctx)
+
+	log.Println("gracefully stopped stopped")
+}
